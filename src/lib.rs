c6343f668f45c13498c7643057dd56ba408dbc5d
Jay Schmidek <jay@schmidek.ca>
2021-06-06 12:56:04 -0700

    Support multiple mem tables

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,83 @@
-use std::collections::BTreeMap;
+mod mem_table;
+
 use std::fs::File;
 use std::sync::RwLock;
+use mem_table::MemTable;
+use core::mem;
+
+#[derive(Clone)]
+pub struct MemTableConfig {
+    pub max_size: usize, // max size of mem table in bytes
+}
+
+impl MemTableConfig {
+    pub fn default() -> MemTableConfig {
+        MemTableConfig {
+            max_size: 1_000_000
+        }
+    }
+
+    pub fn max_size(mut self, size: usize) -> Self {
+        self.max_size = size;
+        self
+    }
+}
 
 struct Database {
-    mem_table: RwLock<BTreeMap<Box<[u8]>, Box<[u8]>>>,
+    mem_table: RwLock<MemTable>,
+    full_mem_tables: RwLock<Vec<MemTable>>,
 }
 
 impl Database {
     fn open() -> Database {
+        Database::open_with_config(MemTableConfig::default())
+    }
+
+    fn open_with_config(mem_table_config: MemTableConfig) -> Database {
         Database {
-            mem_table: RwLock::new(BTreeMap::new()),
+            mem_table: RwLock::new(MemTable::new(mem_table_config)),
+            full_mem_tables: RwLock::new(Vec::new()),
         }
     }
 
     fn insert<K: AsRef<[u8]>, V: AsRef<[u8]>>(&self, key: K, value: V) {
         let mut map = self.mem_table.write().expect("RwLock poisoned");
-        map.insert(
+        while !map.insert(
             key.as_ref().to_vec().into_boxed_slice(),
             value.as_ref().to_vec().into_boxed_slice(),
-        );
+        ){
+            let mut full_maps = self.full_mem_tables.write().expect("RwLock poisoned");
+            let new_map = MemTable::new(map.config.clone());
+            full_maps.push(mem::replace(&mut map, new_map));
+        }
     }
 
     fn get<K: AsRef<[u8]>>(&self, key: K) -> Option<Box<[u8]>> {
         let map = self.mem_table.read().expect("RwLock poisoned");
-        map.get(key.as_ref()).map(|v| v.clone())
+        map.get(key.as_ref())
+            .or_else(|| {
+                let full_maps = self.full_mem_tables.read().expect("RwLock poisoned");
+                for full_map in full_maps.iter() {
+                    let ret = full_map.get(key.as_ref());
+                    if ret.is_some() {
+                        return ret;
+                    }
+                }
+                None
+            })
+            .map(|v| v.clone())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Database;
+    use crate::{Database, mem_table, MemTableConfig};
     use std::sync::Arc;
     use std::thread;
 
     #[test]
     fn basic() {
-        let mut db = Database::open();
+        let db = Database::open();
         let key = vec![8];
         let value = vec![9, 10];
         db.insert(&key, &value);
@@ -45,7 +87,7 @@ mod tests {
 
     #[test]
     fn basic_strings() {
-        let mut db = Database::open();
+        let db = Database::open();
         let key = String::from("key");
         let value = String::from("value");
         db.insert(key.as_bytes(), value.as_bytes());
@@ -71,6 +113,29 @@ mod tests {
 
         for thread in threads {
             let _ = thread.join();
+        }
+    }
+
+    #[test]
+    fn overflow_mem_table(){
+        let db = Database::open_with_config(MemTableConfig::default().max_size(20));
+
+        let first_key = String::from("first_key");
+        let first_value = String::from("first_value");
+        db.insert(first_key.as_bytes(), first_value.as_bytes());
+
+        for i in 0..100 {
+            let key = format!("key{}", i);
+            let value = format!("value{}", i);
+            db.insert(key.as_bytes(), value.as_bytes());
+            assert_eq!(
+                db.get(key.as_bytes()).as_ref().map(|v| &(v[..])),
+                Some(value.as_bytes())
+            );
+            assert_eq!(
+                db.get(first_key.as_bytes()).as_ref().map(|v| &(v[..])),
+                Some(first_value.as_bytes())
+            );
         }
     }
 }
---
+++ src/mem_table.rs
@@ -0,0 +1,39 @@
+use std::collections::BTreeMap;
+use crate::MemTableConfig;
+
+pub struct MemTable {
+    pub config: MemTableConfig,
+    size: usize,
+    entries: BTreeMap<Box<[u8]>, Box<[u8]>>,
+}
+
+impl MemTable {
+    pub fn new(config: MemTableConfig) -> MemTable {
+        MemTable {
+            config,
+            size: 0,
+            entries: BTreeMap::new(),
+        }
+    }
+
+    pub fn insert<K: AsRef<[u8]>, V: AsRef<[u8]>>(&mut self, key: K, value: V) -> bool {
+        let entry_size: usize = key.as_ref().len() + value.as_ref().len();
+        if entry_size > self.config.max_size {
+            panic!("KeyValue can't be larger than max mem table size!");
+        }
+        let new_size = self.size + entry_size;
+        if new_size > self.config.max_size {
+            return false;
+        }
+        self.entries.insert(
+            key.as_ref().to_vec().into_boxed_slice(),
+            value.as_ref().to_vec().into_boxed_slice(),
+        );
+        self.size = new_size;
+        true
+    }
+
+    pub fn get<K: AsRef<[u8]>>(&self, key: K) -> Option<Box<[u8]>> {
+        self.entries.get(key.as_ref()).map(|v| v.clone())
+    }
+}
